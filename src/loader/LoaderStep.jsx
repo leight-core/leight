256c448c6f4b98c7316a3a9eeb47c685b1da6b23
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-27 12:31:45 +0100

    Common: Cleaned up package.json deps

--- src/loader/LoaderStep.jsx
+++ src/loader/LoaderStep.jsx
@@ -1,8 +1,12 @@
 import {Steps} from "antd";
+import PropTypes from "prop-types";
 import React, {useEffect, useState} from "react";
 import Spinner from "../icon/Spinner";
 import {useStepLoaderContext} from "./StepLoaderContext";
 
+/**
+ * Step implementation - when active, an action (onStep) is executed.
+ */
 const LoaderStep = ({onStep, icon, ...props}) => {
 	const [loading, setLoading] = useState(false);
 	const stepLoaderContext = useStepLoaderContext();
@@ -18,6 +22,20 @@ const LoaderStep = ({onStep, icon, ...props}) => {
 			icon={<Spinner done={!loading || stepLoaderContext.status === "error"} children={icon}/>}
 		/>
 	);
+};
+
+/**
+ * @typedef {function} LoaderStepOnStepType
+ * @param {StepLoaderContextType} stepLoaderContext
+ */
+
+LoaderStep.propTypes = {
+	/**
+	 * Step function being executed when this step is action (do not use React Hooks!).
+	 *
+	 * @type {LoaderStepOnStepType}
+	 */
+	onStep: PropTypes.func.isRequired,
 };
 
 export default LoaderStep;
