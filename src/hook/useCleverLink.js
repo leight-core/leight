665e0ef4d7df9fad96bb10117cf4ca8dd8bdadd2
Marek Hanzal <marek.hanzal@x32.cz>
2020-11-30 17:35:43 +0100

    Common: Fixed clever link

--- src/hook/useCleverLink.js
+++ src/hook/useCleverLink.js
@@ -11,7 +11,7 @@ import {useLayoutContext} from "../layout/LayoutContext";
  */
 const useCleverLink = to => {
 	const layoutContext = useLayoutContext();
-	const request = to.matchAll(/:([a-z0-9-]+)/g).map(item => item[1]) || [];
+	const request = Array.from(to.matchAll(/:([a-z0-9-]+)/g)).map(item => item[1]) || [];
 	const params = useParams();
 	const current = omitEmpty({...layoutContext.data, ...params});
 	const diff = diffArray(request, Object.keys(current));
