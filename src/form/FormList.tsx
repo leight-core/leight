92fe6ba7cedec91234cf89b938607851f91aea6e
Marek Hanzal <marek.hanzal@x32.cz>
2020-12-02 21:20:43 +0100

    Common: Fixed types

--- src/form/FormList.tsx
+++ src/form/FormList.tsx
@@ -1,7 +1,12 @@
 import {Form} from "antd";
-import React from "react";
+import {FormListProps} from "antd/lib/form";
+import {NamePath} from "rc-field-form/lib/interface";
+import React, {FC} from "react";
 
-export const FormList = (props) => {
-	// noinspection RequiredAttributes
-	return <Form.List {...props}/>;
+export interface IFormList extends Partial<FormListProps> {
+	field: NamePath
+}
+
+export const FormList: FC<IFormList> = ({field, ...props}) => {
+	return <Form.List name={field} {...props} children={props.children}/>;
 };
