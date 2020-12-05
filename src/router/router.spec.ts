0b6eea6fc21e2a6bde926f36f1972cf2315540f9
Marek Hanzal <marek.hanzal@x32.cz>
2020-12-05 14:27:03 +0100

    Common: Improved test

--- src/router/router.spec.ts
+++ src/router/router.spec.ts
@@ -2,4 +2,7 @@ import {link} from "./router";
 
 test("Request unknown Link ID", () => {
 	expect(() => link("foo")).toThrow("Requested unknown link with id [foo]. Did you register it?");
+	link("prefix", "/the/root");
+	link("foo", "prefix:/bar");
+	expect(link("foo")).toBe("/the/root/bar");
 });
