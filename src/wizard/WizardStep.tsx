8733cba098e5b1ec2459e4f31dec37751775f976
Marek Hanzal <marek.hanzal@x32.cz>
2020-12-14 18:24:10 +0100

    Common: Improved wizard step

--- src/wizard/WizardStep.tsx
+++ src/wizard/WizardStep.tsx
@@ -1,5 +1,6 @@
 import {Divider} from "antd";
 import {FC} from "react";
+import {ScrollToTop} from "../component/ScrollToTop";
 import {useModuleContext} from "../module/ModuleContext";
 
 export interface IWizardStepProps {
@@ -10,6 +11,7 @@ export const WizardStep: FC<IWizardStepProps> = ({title, children}) => {
 	const moduleContext = useModuleContext();
 	return (
 		<>
+			<ScrollToTop/>
 			<h2>{moduleContext.t(title)}</h2>
 			<Divider type={"horizontal"}/>
 			{children}
