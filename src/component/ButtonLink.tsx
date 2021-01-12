aa0b2b3d2e6190470ea807344b34827d347d31e3
Marek Hanzal <marek.hanzal@x32.cz>
2021-01-12 17:06:26 +0100

    Common: Improved button link

--- src/component/ButtonLink.tsx
+++ src/component/ButtonLink.tsx
@@ -2,6 +2,7 @@ import {Button} from "antd";
 import {ButtonProps} from "antd/lib/button";
 import React from "react";
 import {useTranslation} from "react-i18next";
+import {Params} from "react-router";
 import {Link} from "react-router-dom";
 import {useLayoutContext} from "../layout/LayoutContext";
 import {useRouterContext} from "../router/RouterContext";
@@ -15,15 +16,19 @@ export interface IButtonLinkProps extends ButtonProps {
 	 * Title of a button.
 	 */
 	title: string
+	/**
+	 * Optional params for the link generator.
+	 */
+	params?: Params
 }
 
-export const ButtonLink = ({href, title, ...props}) => {
+export const ButtonLink = ({href, title, params, ...props}) => {
 	const {t} = useTranslation();
 	const routerContext = useRouterContext();
 	const layoutContext = useLayoutContext();
 	try {
 		return (
-			<Link to={routerContext.generate(href, layoutContext.data)}>
+			<Link to={routerContext.generate(href, {...layoutContext.data, ...params})}>
 				<Button type={"primary"} children={t(title)} {...props}/>
 			</Link>
 		);
