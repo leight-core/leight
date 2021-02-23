1388c8fdbafdcfa164dcb8bc6d088901c878f98d
Marek Hanzal <marek.hanzal@x32.cz>
2021-02-23 18:13:21 +0100

    Common: Added useInterval method

---
+++ src/utils/useInterval.ts
@@ -0,0 +1,22 @@
+import {useEffect, useRef} from "react";
+
+export type IIntervalCallback = () => any
+
+export function useInterval(callback: IIntervalCallback, delay: number | null, deps: any[] = []) {
+	const ref = useRef<IIntervalCallback>(() => null);
+
+	useEffect(() => {
+		ref.current = callback;
+	}, [callback]);
+
+	useEffect(() => {
+		function tick() {
+			ref.current();
+		}
+
+		if (delay !== null) {
+			const id = setInterval(tick, delay);
+			return () => clearInterval(id);
+		}
+	}, [delay].concat(deps));
+}
