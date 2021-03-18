8d74942d6f0bb81a1e3d142a837af77c70241e5d
Marek Hanzal <marek.hanzal@x32.cz>
2021-03-18 16:00:04 +0100

    Common: Another dayjs helper

--- src/date/utils.ts
+++ src/date/utils.ts
@@ -3,3 +3,7 @@ import dayjs, {ConfigType} from "dayjs";
 export const toLocalDate = (input?: ConfigType, fallback: string = "-"): string => {
 	return input ? dayjs(input).format("L") : fallback;
 };
+
+export const asDayjs = (input?: ConfigType, fallback: ConfigType | null = null) => {
+	return input ? dayjs(input) : (fallback ? (dayjs(fallback)) : null);
+};
