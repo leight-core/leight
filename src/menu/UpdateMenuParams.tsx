1fcaabdfbfbd0822b99bbe70d825095bcca5d3da
Marek Hanzal <marek.hanzal@x32.cz>
2021-03-20 12:00:24 +0100

    Common: Fixed update menu

--- src/menu/UpdateMenuParams.tsx
+++ src/menu/UpdateMenuParams.tsx
@@ -1,20 +1,20 @@
-import {useEffect} from "react";
+import {ReactNode, useEffect} from "react";
 import {Params} from "react-router";
 import {useMenuContext} from "./MenuContext";
 
-export interface IUpdateMenuParamsProps<TParams extends Params = any, TChildren = JSX.Element> {
+export interface IUpdateMenuParamsProps<TParams extends Params = any> {
 	params: TParams
-	children: TChildren
+	children: ReactNode
 }
 
 /**
  * Forces redraw of components using MenuContext (thus be careful when using in data fetching and other async
  * components to prevent infinite loop).
  */
-export const UpdateMenuParams = <TParams extends Params, TChildren>({params, children}: IUpdateMenuParamsProps<TParams, TChildren>): TChildren => {
+export const UpdateMenuParams = <TParams extends Params>({params, children}: IUpdateMenuParamsProps<TParams>) => {
 	const menuContext = useMenuContext();
 	useEffect(() => {
 		menuContext.setParams(params);
 	}, [params]);
-	return children;
+	return <>{children}</>;
 };
