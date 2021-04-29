5095c3a7dfdd66f8e894a17f04e1df8e06e08336
Marek Hanzal <marek.hanzal@x32.cz>
2021-04-29 14:52:52 +0200

    Common: A little improvement

--- src/date/utils.ts
+++ src/date/utils.ts
@@ -1,13 +1,13 @@
 import dayjs, {ConfigType} from "dayjs";
 
-export const toLocalDate = (input?: ConfigType, fallback: string = "-"): string => {
+export const toLocalDate = (input?: ConfigType | null, fallback: string = "-"): string => {
 	return input ? dayjs(input).format("L") : fallback;
 };
 
-export const toLocalDateTime = (input?: ConfigType, fallback: string = "-"): string => {
+export const toLocalDateTime = (input?: ConfigType | null, fallback: string = "-"): string => {
 	return input ? dayjs(input).format("L LTS") : fallback;
 };
 
-export const asDayjs = (input?: ConfigType, fallback: ConfigType | null = null) => {
+export const asDayjs = (input?: ConfigType | null, fallback: ConfigType | null = null) => {
 	return input ? dayjs(input) : (fallback ? (dayjs(fallback)) : null);
 };
