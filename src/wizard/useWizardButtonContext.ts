44569a230c29de63c778b5ff50001cdc992f786e
Marek Hanzal <marek.hanzal@x32.cz>
2021-05-13 15:14:13 +0200

    Common: Docs

--- src/wizard/useWizardButtonContext.ts
+++ src/wizard/useWizardButtonContext.ts
@@ -1,4 +1,7 @@
 import {useButtonContext} from "../button/ButtonContext";
 import {IWizardButton} from "./interface";
 
+/**
+ * Provides an access to buttons in a Wizard component; could be used just from any step component.
+ */
 export const useWizardButtonContext = () => useButtonContext<IWizardButton>();
