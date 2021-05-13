c4b0b449f10ede1b4de123099756ca07131506f1
Marek Hanzal <marek.hanzal@x32.cz>
2021-05-13 23:42:00 +0200

    Common: Fixed missing Wizard exports

--- src/wizard/index.ts
+++ src/wizard/index.ts
@@ -1,4 +1,6 @@
 export * from "./interface";
+export * from "./useStepEvents";
+export * from "./useWizardButtonContext";
 export * from "./Wizard";
 export * from "./WizardContext";
 export * from "./WizardEvents";
