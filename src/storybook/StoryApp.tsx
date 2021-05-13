67553f8544fe4130d8beb3b13bbcac7b194bc12b
Marek Hanzal <marek.hanzal@x32.cz>
2021-05-14 01:16:49 +0200

    Common: Fixed storybook

--- src/storybook/StoryApp.tsx
+++ src/storybook/StoryApp.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {AppContext} from "../app/AppContext";
+import {BlockContextProvider} from "../block/BlockContextProvider";
 import {DiscoveryContextProvider} from "../discovery/DiscoveryContextProvider";
 import {LayoutContext} from "../layout/LayoutContext";
 import {RouterContext} from "../router/RouterContext";
@@ -8,17 +9,19 @@ import {ViewContext} from "../view/ViewContext";
 export const StoryApp = ({children}) => {
 	return (
 		<AppContext.Provider value={{appContext: true} as unknown as any}>
-			<DiscoveryContextProvider>
-				<RouterContext.Provider value={{
-					useNavigate: () => null,
-				} as unknown as any}>
-					<LayoutContext.Provider value={{} as unknown as any}>
-						<ViewContext.Provider value={{} as unknown as any}>
-							{children}
-						</ViewContext.Provider>
-					</LayoutContext.Provider>
-				</RouterContext.Provider>
-			</DiscoveryContextProvider>
+			<BlockContextProvider>
+				<DiscoveryContextProvider>
+					<RouterContext.Provider value={{
+						useNavigate: () => null,
+					} as unknown as any}>
+						<LayoutContext.Provider value={{} as unknown as any}>
+							<ViewContext.Provider value={{} as unknown as any}>
+								{children}
+							</ViewContext.Provider>
+						</LayoutContext.Provider>
+					</RouterContext.Provider>
+				</DiscoveryContextProvider>
+			</BlockContextProvider>
 		</AppContext.Provider>
 	);
 };
