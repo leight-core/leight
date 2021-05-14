7f7ffdaaf3fd0db366b9e37a3f38c0fac60db2dc
Marek Hanzal <marek.hanzal@x32.cz>
2021-05-14 11:13:53 +0200

    Common: Added another date util

--- src/date/utils.ts
+++ src/date/utils.ts
@@ -8,6 +8,15 @@ export const toLocalDateTime = (input?: ConfigType | null, fallback: string = "-
 	return input ? dayjs(input).format("L LTS") : fallback;
 };
 
+export const toUtcDateTime = (input ?: ConfigType | null, fallback: string | null = null): string | null => {
+	try {
+		return input ? (dayjs(input) as any).utc().format() : fallback;
+	} catch (e) {
+		console.error("Dayjs does not have registered utc() plugin!", "https://day.js.org/docs/en/plugin/utc", e);
+		return fallback;
+	}
+};
+
 export const asDayjs = (input?: ConfigType | null, fallback: ConfigType | null = null) => {
 	return input ? dayjs(input) : (fallback ? (dayjs(fallback)) : null);
 };
