11eb182b0be77a317f4c7b0a9f2c24e1230c414b
Marek Hanzal <marek.hanzal@x32.cz>
2021-07-23 07:38:41 +0200

    Common: Added title just to a page

--- src/page/Page.tsx
+++ src/page/Page.tsx
@@ -1,4 +1,6 @@
+import Head from "next/head";
 import {FC} from "react";
+import {useTranslation} from "react-i18next";
 import {ScrollToTop} from "../component/ScrollToTop";
 
 export interface IPageProps {
@@ -6,7 +8,9 @@ export interface IPageProps {
 }
 
 export const Page: FC<IPageProps> = ({title, children}) => {
+	const {t} = useTranslation();
 	return <>
+		{title && <Head><title key={"title"}>{title ? t(title + ".title") : title}</title></Head>}
 		<ScrollToTop/>
 		{children}
 	</>;
