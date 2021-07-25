7fad06b9df13e5d3495f560e2f53443771305f3e
Marek Hanzal <marek.hanzal@x32.cz>
2021-07-25 18:43:22 +0200

    Common: Improved base page stuff

--- src/page/Page.tsx
+++ src/page/Page.tsx
@@ -1,17 +1,35 @@
+import {Card} from "antd";
 import Head from "next/head";
-import {FC} from "react";
+import {FC, ReactNode} from "react";
 import {useTranslation} from "react-i18next";
 import {ScrollToTop} from "../component/ScrollToTop";
+import {UpdatePageHeader} from "../layout/UpdatePageHeader";
 
 export interface IPageProps {
+	/**
+	 * Optional header (if used, UpdateHeader component will be executed).
+	 */
+	header?: ReactNode;
+	/**
+	 * The page title (main title); it goes through translation.
+	 */
+	h1: string;
+	/**
+	 * If provided, page title will be updated (tab name). Must be explicitly provided to change a title.
+	 */
 	title?: string;
 }
 
-export const Page: FC<IPageProps> = ({title, children}) => {
+export const Page: FC<IPageProps> = ({header, title, h1, children}) => {
 	const {t} = useTranslation();
 	return <>
+		{header && <UpdatePageHeader>
+			{header}
+		</UpdatePageHeader>}
 		{title && <Head><title key={"title"}>{title ? t(title + ".title") : title}</title></Head>}
 		<ScrollToTop/>
-		{children}
+		<Card title={t(h1)}>
+			{children}
+		</Card>
 	</>;
 };
