f491a23b922eaf0e8c06064a1499eca6a29b8cc5
Marek Hanzal <marek.hanzal@x32.cz>
2021-07-26 08:43:14 +0200

    Common: Renamed LinkTo property

--- src/link/LinkTo.tsx
+++ src/link/LinkTo.tsx
@@ -7,7 +7,7 @@ export interface ILinkToProps {
 	/**
 	 * Target href; could be template path, will be expanded by a LinkContext.
 	 */
-	to: string;
+	href: string;
 	/**
 	 * Optional params used to generate a link.
 	 */
@@ -17,9 +17,9 @@ export interface ILinkToProps {
 /**
  * Wrapper component over Next.js Link with underlying <a> with children passed through.
  */
-export const LinkTo: FC<ILinkToProps> = ({to, params, children}) => {
+export const LinkTo: FC<ILinkToProps> = ({href, params, children}) => {
 	const linkContext = useLinkContext();
-	return <Link href={linkContext.generate(to, params)}>
+	return <Link href={linkContext.generate(href, params)}>
 		<a>{children}</a>
 	</Link>;
 };
