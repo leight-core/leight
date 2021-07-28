871880362daee2afaecc80fda3530c540f91ed3e
Marek Hanzal <marek.hanzal@x32.cz>
2021-07-28 14:39:16 +0200

    Common: Reverted LinkTo change

--- src/link/LinkTo.tsx
+++ src/link/LinkTo.tsx
@@ -18,6 +18,6 @@ export interface ILinkToProps {
  */
 export const LinkTo: FC<ILinkToProps> = ({href, params, children}) => {
 	return <Link href={{pathname: href, query: params}}>
-		{children}
+		<a>{children}</a>
 	</Link>;
 };
