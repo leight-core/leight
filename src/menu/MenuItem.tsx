6b148d9b1437632ba0b56638fdd73fc51937ce14
Marek Hanzal <marek.hanzal@x32.cz>
2021-07-28 16:24:26 +0200

    Common: MenuItem requires title and href separately

--- src/menu/MenuItem.tsx
+++ src/menu/MenuItem.tsx
@@ -1,39 +1,37 @@
 import {Menu, MenuItemProps} from "antd";
-import Link from "next/link";
 import {FC, ReactNode} from "react";
 import {useTranslation} from "react-i18next";
-import {useCleverLink} from "../hook/useCleverLink";
 import {IParams} from "../link/interface";
+import {LinkTo} from "../link/LinkTo";
 import {useParamContext} from "../param/ParamContext";
 
 export interface IMenuItemProps extends Partial<MenuItemProps> {
 	/**
-	 * Menu ID used for translations, link and others.
+	 * Menu item title, goes through translation.
 	 */
-	id: string;
+	title: string;
+	/**
+	 * Menu item href, goes through a link generator.
+	 */
+	href: string;
 	/**
 	 * Menu icon
 	 */
 	icon: ReactNode;
-	/**
-	 * Optional href (link id) if id should not be used.
-	 */
-	href?: string;
 	/**
 	 * Optional params for link generator.
 	 */
 	params?: IParams;
 }
 
-export const MenuItem: FC<IMenuItemProps> = ({id, icon, href, params, ...props}) => {
+export const MenuItem: FC<IMenuItemProps> = ({title, href, icon, params, ...props}) => {
 	const paramContext = useParamContext();
 	const {t} = useTranslation();
-	const cleverLink = useCleverLink(href || id, {...paramContext.params, ...params});
 	return (
-		<Menu.Item icon={icon} key={id} {...props} disabled={!cleverLink.enable}>
-			<Link href={cleverLink.link}>
-				<a>{t(id + ".menu")}</a>
-			</Link>
+		<Menu.Item icon={icon} key={href} {...props}>
+			<LinkTo href={href} params={{...paramContext.params, ...params}}>
+				{t(title)}
+			</LinkTo>
 		</Menu.Item>
 	);
 };
@@ -44,8 +42,6 @@ export const MenuItem: FC<IMenuItemProps> = ({id, icon, href, params, ...props})
  *
  * Basically it has the same behavior as MenuItem component.
  */
-export function CreateMenuItem(id: string, icon: ReactNode, params?: IParams, rest?: Partial<IMenuItemProps>) {
-	return (
-		<MenuItem id={id} key={id} icon={icon} params={params} {...rest}/>
-	);
+export function CreateMenuItem(title: string, href: string, icon: ReactNode, params?: IParams, rest?: Partial<IMenuItemProps>) {
+	return <MenuItem title={title} key={href} href={href} icon={icon} params={params} {...rest}/>;
 }
