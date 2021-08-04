5cfaca0e8db70eb25b1a02ed4f24c307d4ac89fe
Marek Hanzal <marek.hanzal@x32.cz>
2021-08-04 11:42:06 +0200

    Common: Another loader

--- src/page/LoadingPage.tsx
+++ src/page/LoadingPage.tsx
@@ -1,13 +1,13 @@
-import {SettingFilled} from "@ant-design/icons";
 import {Result} from "antd";
 import {FC, ReactNode} from "react";
+import {LoaderIcon} from "../icon/LoaderIcon";
 import {EmptyPage, IEmptyPageProps} from "./EmptyPage";
 
 export interface ILoadingPageProps extends Partial<IEmptyPageProps> {
 	icon?: ReactNode;
 }
 
-export const LoadingPage: FC<ILoadingPageProps> = ({children, icon = <SettingFilled spin style={{fontSize: 42}}/>, ...props}) => {
+export const LoadingPage: FC<ILoadingPageProps> = ({children, icon = <LoaderIcon style={{fontSize: 42}}/>, ...props}) => {
 	return <EmptyPage name={"common.loading"} {...props}>
 		<Result
 			style={{marginTop: "10vh"}}
