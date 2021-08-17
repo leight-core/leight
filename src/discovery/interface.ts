322e5695f3df1ccfcc1ea8b9faef988e7a4958ea
Marek Hanzal <marek.hanzal@x32.cz>
2021-08-17 12:30:38 +0200

    Common: Updated discovery interface

--- src/discovery/interface.ts
+++ src/discovery/interface.ts
@@ -4,37 +4,41 @@ export interface IDiscoveryContext {
 	/**
 	 * Set current discovery index.
 	 */
-	setDiscovery: (discovery: IDiscovery) => void
+	setDiscovery: (discovery: IDiscovery) => void;
 	/**
 	 * Generate link from the given discovery index ID. Or throws an error if
 	 * the ID does not exist.
 	 */
-	link: (id: string, params ?: IParams) => string
+	link: (id: string, params ?: IParams) => string;
 }
 
 export interface IDiscoveryItem {
 	/**
 	 * ID of this discovery item; should be simply rememberable.
 	 */
-	id: string
+	id: string;
 	/**
 	 * Link on the server side (thus discovery index id is an interface between the
 	 * client application and remote application).
 	 */
-	href: string
+	link: string;
+	/**
+	 * Just an URL part of the link.
+	 */
+	url: string;
 	/**
 	 * Optional description of an endpoint.
 	 */
-	description: string | null
+	description: string | null;
 }
 
 export interface IDiscoveryIndex {
-	[index: string]: IDiscoveryItem
+	[index: string]: IDiscoveryItem;
 }
 
 /**
  * Discovery index. Simple map of discovery index id and discovery item.
  */
 export interface IDiscovery {
-	index: IDiscoveryIndex
+	index: IDiscoveryIndex;
 }
