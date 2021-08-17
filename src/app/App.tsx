faaccce4f83fab2a2b9fe52b944bfd365131e353
Marek Hanzal <marek.hanzal@x32.cz>
2021-08-17 19:55:14 +0200

    Common: Simplified App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,5 @@
-import {AntDesignOutlined} from "@ant-design/icons";
-import {AppContextProvider, ClientContextProvider, DiscoveryContextProvider, IServerEvents, LinkContextProvider, LoadingPage, SessionContextProvider, TranslationLoader, useAppContext} from "@leight-core/leight";
-import {Result} from "antd";
-import {FC, ReactNode, Suspense} from "react";
+import {ClientContextProvider, DiscoveryContextProvider, LinkContextProvider, SessionContextProvider, TranslationLoader} from "@leight-core/leight";
+import {FC} from "react";
 
 export interface IAppProps {
 	/**
@@ -14,63 +12,31 @@ export interface IAppProps {
 	 * Optional href to obtain user ticket (user session) when app starts; defaults to "public.user.user-ticket".
 	 */
 	sessionLink?: string;
-	/**
-	 * Optional icon shown when an application bootstraps.
-	 */
-	icon?: ReactNode;
-	/**
-	 * Optional hook for session (session ticket) events.
-	 */
-	sessionEvents?: IServerEvents;
 }
-
-const AppInternal: FC<IAppProps> = ({icon, children}) => {
-	const appContext = useAppContext();
-	return <>
-		{appContext.isReady ?
-			<Suspense fallback={<LoadingPage/>}>
-				{children}
-			</Suspense> :
-			<Result icon={icon || <AntDesignOutlined/>}>
-				<div style={{display: "flex", justifyContent: "center"}}>
-					{children}
-				</div>
-			</Result>
-		}
-	</>;
-};
 
 /**
  * Common default Application:
  *
+ * - uses default server-side config loading (for discovery)
  * - uses server-side discovery by default
  * - uses server-side translations by default (with a setup of i18n)
+ * - uses server-side default user login check
  */
 export const App: FC<IAppProps> = (
 	{
 		clientHref = process.env.NEXT_PUBLIC_CLIENT,
 		sessionLink,
-		sessionEvents,
-		icon,
 		children,
 	}) => {
-	return <AppContextProvider>
-		<LinkContextProvider>
-			<ClientContextProvider href={clientHref}>
-				<DiscoveryContextProvider>
-					<TranslationLoader>
-						<SessionContextProvider link={sessionLink}>
-							<AppInternal
-								clientHref={clientHref}
-								sessionLink={sessionLink}
-								sessionEvents={sessionEvents}
-								icon={icon}
-								children={children}
-							/>
-						</SessionContextProvider>
-					</TranslationLoader>
-				</DiscoveryContextProvider>
-			</ClientContextProvider>
-		</LinkContextProvider>
-	</AppContextProvider>;
+	return <LinkContextProvider>
+		<ClientContextProvider href={clientHref}>
+			<DiscoveryContextProvider>
+				<TranslationLoader>
+					<SessionContextProvider link={sessionLink}>
+						{children}
+					</SessionContextProvider>
+				</TranslationLoader>
+			</DiscoveryContextProvider>
+		</ClientContextProvider>
+	</LinkContextProvider>;
 };
