bdfa50522473af38e386d67f32d4958bba0c10e2
Marek Hanzal <marek.hanzal@x32.cz>
2021-08-25 18:28:18 +0200

    Common: Changed loader icon...again

--- src/icon/LoaderIcon.tsx
+++ src/icon/LoaderIcon.tsx
@@ -1,4 +1,5 @@
-import {Spin, SpinProps} from "antd";
+import {SyncOutlined} from "@ant-design/icons";
 import React, {FC} from "react";
+import {IIconProps} from "./interface";
 
-export const LoaderIcon: FC<SpinProps> = props => <Spin size={"large"} {...props}/>;
+export const LoaderIcon: FC<IIconProps> = props => <SyncOutlined spin {...props}/>;
