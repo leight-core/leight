c1b03265102f66bbefe2c1408d2f4e8b0d4d2885
Marek Hanzal <marek.hanzal@x32.cz>
2021-09-01 21:02:10 +0200

    Common: Version bump

--- src/list/List.tsx
+++ src/list/List.tsx
@@ -12,6 +12,7 @@ export const List = <TItem extends Object = IRecordItem>(
 		...props
 	}: IListProps<TItem>) => {
 	const dataSourceContext = useDataSourceContext<TItem>();
+
 	// useEffect(() => onPage(0, dataSourceContext.size).cleaner(), deps);
 
 	return <CoolList
