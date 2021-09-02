33c2cf31ebc3c1cd3f4a7fc69c2d11d370b0c813
Marek Hanzal <marek.hanzal@x32.cz>
2021-09-03 01:14:37 +0200

    Common: Just improvement

--- src/data/OrderButtonBar.tsx
+++ src/data/OrderButtonBar.tsx
@@ -2,12 +2,13 @@ import {CancelOrderButton, OrderButton} from "@leight-core/leight";
 import {Divider, Space, SpaceProps} from "antd";
 
 export interface IOrderButtonBarProps<TOrderBy> extends Partial<SpaceProps> {
+	showCancelButton?: boolean;
 	buttons: (keyof TOrderBy)[];
 }
 
-export const OrderButtonBar = <TOrderBy, >({buttons, ...props}: IOrderButtonBarProps<TOrderBy>) => {
+export const OrderButtonBar = <TOrderBy, >({showCancelButton = true, buttons, ...props}: IOrderButtonBarProps<TOrderBy>) => {
 	return <Space split={<Divider type={"vertical"}/>} size={"small"} {...props}>
 		{buttons.map(item => <OrderButton<TOrderBy> key={item as string} orderBy={item}/>)}
-		<CancelOrderButton/>
+		{showCancelButton && <CancelOrderButton/>}
 	</Space>;
 };
