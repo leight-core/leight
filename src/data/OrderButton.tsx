0026cdcc00af5b187852ec00179915cb698d6b91
Marek Hanzal <marek.hanzal@x32.cz>
2021-09-03 03:20:08 +0200

    Common: Another improvement

--- src/data/OrderButton.tsx
+++ src/data/OrderButton.tsx
@@ -5,12 +5,14 @@ import {useTranslation} from "react-i18next";
 
 export interface IOrderButtonProps<TOrderBy> {
 	orderBy: keyof TOrderBy;
+	prefix: string;
 }
 
-export const OrderButton = <TOrderBy, >({orderBy}: IOrderButtonProps<TOrderBy>) => {
+export const OrderButton = <TOrderBy, >({orderBy, prefix}: IOrderButtonProps<TOrderBy>) => {
 	const dataSourceContext = useDataSourceContext<any, any>();
 	const order = dataSourceContext.orderBy ? dataSourceContext.orderBy[orderBy] : undefined;
 	const {t} = useTranslation();
+	const label = t("order-by.label." + prefix + "." + orderBy);
 	return <Space size={"small"}>
 		<Button
 			icon={
@@ -22,9 +24,9 @@ export const OrderButton = <TOrderBy, >({orderBy}: IOrderButtonProps<TOrderBy>) 
 			size={"small"}
 			onClick={() => dataSourceContext.setOrderBy({[orderBy]: !order})}
 		>
-			{(order === undefined && <Tooltip title={t("common.order.undefined.tooltip")}>{t("order-by.label." + orderBy)}</Tooltip>) ||
-			(order === true && <Tooltip title={t("common.order.ascending.tooltip")}>{t("order-by.label." + orderBy)}</Tooltip>) ||
-			(order === false && <Tooltip title={t("common.order.descending.tooltip")}>{t("order-by.label." + orderBy)}</Tooltip>)}
+			{(order === undefined && <Tooltip title={t("common.order.undefined.tooltip")}>{label}</Tooltip>) ||
+			(order === true && <Tooltip title={t("common.order.ascending.tooltip")}>{label}</Tooltip>) ||
+			(order === false && <Tooltip title={t("common.order.descending.tooltip")}>{label}</Tooltip>)}
 		</Button>
 	</Space>;
 };
--- src/data/OrderButtonBar.tsx
+++ src/data/OrderButtonBar.tsx
@@ -4,11 +4,12 @@ import {Divider, Space, SpaceProps} from "antd";
 export interface IOrderButtonBarProps<TOrderBy> extends Partial<SpaceProps> {
 	showCancelButton?: boolean;
 	buttons: (keyof TOrderBy)[];
+	prefix: string;
 }
 
-export const OrderButtonBar = <TOrderBy, >({showCancelButton = true, buttons, ...props}: IOrderButtonBarProps<TOrderBy>) => {
+export const OrderButtonBar = <TOrderBy, >({showCancelButton = true, buttons, prefix, ...props}: IOrderButtonBarProps<TOrderBy>) => {
 	return <Space split={<Divider type={"vertical"}/>} size={"small"} {...props}>
-		{buttons.map(item => <OrderButton<TOrderBy> key={item as string} orderBy={item}/>)}
+		{buttons.map(item => <OrderButton<TOrderBy> prefix={prefix} key={item as string} orderBy={item}/>)}
 		{showCancelButton && <CancelOrderButton/>}
 	</Space>;
 };
