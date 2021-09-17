2651f7dfcd97305459aca0e12488190e7440882a
Marek Hanzal <marek.hanzal@x32.cz>
2021-09-17 18:03:40 +0200

    Common: Fixed default open keys

--- src/menu/Menu.tsx
+++ src/menu/Menu.tsx
@@ -1,31 +1,28 @@
 import {useMenuContext} from "@leight-core/leight";
 import {Menu as CoolMenu, MenuProps} from "antd";
 import {useRouter} from "next/router";
-import React, {FC, useEffect, useState} from "react";
+import React, {FC} from "react";
 
 export interface IMenuProps extends Partial<MenuProps> {
 }
 
 export const Menu: FC<IMenuProps> = props => {
 	const menuContext = useMenuContext();
-	const [items, setItems] = useState<string[]>([]);
 	const router = useRouter();
-
-	useEffect(() => {
-		const current: string[] = [];
-		const selected: string[] = [];
-		router.route.substr(1).split("/").forEach(item => {
-			current.push(item);
-			selected.push(current.join("."));
-		});
-		setItems(selected);
-	}, [router.route]);
 
 	return <CoolMenu
 		mode={"inline"}
 		selectable={true}
 		selectedKeys={menuContext.current}
-		defaultOpenKeys={items}
+		defaultOpenKeys={(() => {
+			const current: string[] = [];
+			const items: string[] = [];
+			router.route.substr(1).split("/").forEach(item => {
+				current.push(item);
+				items.push(current.join("."));
+			});
+			return items;
+		})()}
 		subMenuCloseDelay={0.35}
 		{...props}
 	/>;
