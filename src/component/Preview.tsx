7c2371249e39d50cb9af2c7cf7e138557256f627
Marek Hanzal <marek.hanzal@x32.cz>
2021-09-18 18:03:38 +0200

    Common: Improved preview again

--- src/component/Preview.tsx
+++ src/component/Preview.tsx
@@ -4,10 +4,11 @@ import {useTranslation} from "react-i18next";
 
 export interface IPreviewProps extends Partial<DescriptionsProps> {
 	width?: number;
+	translation?: string;
 	children: { [index in string]: ReactNode };
 }
 
-export const Preview: FC<IPreviewProps> = ({width = 220, children, ...props}) => {
+export const Preview: FC<IPreviewProps> = ({width = 220, translation, children, ...props}) => {
 	const {t} = useTranslation();
 	return <Descriptions
 		bordered
@@ -16,7 +17,7 @@ export const Preview: FC<IPreviewProps> = ({width = 220, children, ...props}) =>
 		labelStyle={{width: width + "px"}}
 		{...props}
 	>
-		{Object.entries(children).map(([key, item]) => <Descriptions.Item key={key} label={t(key)}>
+		{Object.entries(children).map(([key, item]) => <Descriptions.Item key={key} label={t(translation ? translation + "." + key : key)}>
 			{item ?? "-"}
 		</Descriptions.Item>)}
 	</Descriptions>;
