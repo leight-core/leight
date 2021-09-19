2d7f2e308d2946e5756cc92e46857ae8a5d56dea
Marek Hanzal <marek.hanzal@x32.cz>
2021-09-19 14:48:31 +0200

    Common: Added delete template

---
+++ src/template/DeleteTemplate.tsx
@@ -0,0 +1,13 @@
+import {isString, ITemplateProps, Template} from "@leight-core/leight";
+import {FC} from "react";
+
+export interface IDeleteTemplateProps extends ITemplateProps {
+}
+
+export const DeleteTemplate: FC<IDeleteTemplateProps> = ({title, translation = true, ...props}) => {
+	return <Template
+		title={(translation && isString(title)) ? title + ".delete" : title}
+		translation={translation}
+		{...props}
+	/>;
+};
--- src/template/index.ts
+++ src/template/index.ts
@@ -1,4 +1,5 @@
 export * from "./CreateTemplate";
+export * from "./DeleteTemplate";
 export * from "./EditTemplate";
 export * from "./PreviewTemplate";
 export * from "./Template";
