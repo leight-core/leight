c7917b276c98fb77f78779fffc72906097a78cb7
Marek Hanzal <marek.hanzal@x32.cz>
2021-09-20 21:57:56 +0200

    Common: Figuring out, how to improve axios stuff and remove Events at all

--- src/request/createPut.ts
+++ src/request/createPut.ts
@@ -1,20 +1,19 @@
-import {axiosHandler, IQueryOptions, IQueryParams, IRequestHookCallback, IRequestHookResult, RequestEvents, useLinkContext, useOptionalDiscoveryContext} from "@leight-core/leight";
+import {IQueryOptions, IQueryParams, IRequestHookCallback, useLinkContext, useOptionalDiscoveryContext} from "@leight-core/leight";
 import axios, {AxiosRequestConfig} from "axios";
 import {useQuery} from "react-query";
 
 export function createPutHook<TQuery extends IQueryParams = IQueryParams, TRequest = any, TResponse = any>(link: string): IRequestHookCallback<TQuery, TRequest, TResponse> {
-	return (request: TRequest, query?: TQuery, options?: IQueryOptions<TResponse>, config?: AxiosRequestConfig): IRequestHookResult<TResponse> => {
-		const events = RequestEvents<TResponse>();
-		const linkContext = useLinkContext();
-		const discoveryContext = useOptionalDiscoveryContext();
-		const result = useQuery([link, {query, config}], () => {
-			return new Promise<TResponse>((resolve, reject) => {
-				axiosHandler(config => axios.put<TResponse>(linkContext.link(link, query, discoveryContext), request, config), config)
-					.on("response", resolve)
-					.on("error", reject)
-					.chain(events);
-			});
-		}, options);
-		return {events, result};
+	return (request: TRequest, query?: TQuery, options?: IQueryOptions<TResponse>, config?: AxiosRequestConfig) => {
+		return useQuery([link, {query, config}], usePutPromise<TQuery, TRequest, TResponse>(link, request, query, options), options);
 	};
+}
+
+export function usePutPromise<TQuery extends IQueryParams = IQueryParams, TRequest = any, TResponse = any>(link: string, request: TRequest, query?: TQuery, options?: IQueryOptions<TResponse>, config?: AxiosRequestConfig) {
+	const linkContext = useLinkContext();
+	const discoveryContext = useOptionalDiscoveryContext();
+	return () => new Promise<TResponse>((resolve, reject) => {
+		axios.put<TResponse>(linkContext.link(link, query, discoveryContext), request, config)
+			.then(result => resolve(result.data))
+			.catch(reject);
+	});
 }
