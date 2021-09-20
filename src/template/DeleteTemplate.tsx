07540448aaf7654d848d7d7ea3e3f17d8b7ccc30
Marek Hanzal <marek.hanzal@x32.cz>
2021-09-20 09:30:59 +0200

    Common: Improved delete template a bit

--- src/template/DeleteTemplate.tsx
+++ src/template/DeleteTemplate.tsx
@@ -1,14 +1,52 @@
-import {isString, ITemplateProps, Template} from "@leight-core/leight";
+import {DeleteItemIcon, IDeleteCallback, IQuery, isString, ITemplateProps, Template, useBlockContext, useDiscoveryContext, useNavigate} from "@leight-core/leight";
+import {Button, Divider, message} from "antd";
 import {FC} from "react";
+import {useTranslation} from "react-i18next";
 
 export interface IDeleteTemplateProps extends ITemplateProps {
+	onDelete: IDeleteCallback;
+	deleteQuery?: IQuery;
+	/**
+	 * Where to navigate on successful delete.
+	 */
+	navigateTo: string;
+	navigateQuery?: IQuery;
+	/**
+	 * Base translation for delete messages (.delete.success, .delete.submit, and .delete.error).
+	 */
+	deleteTranslation: string;
 }
 
-export const DeleteTemplate: FC<IDeleteTemplateProps> = ({title, translation = true, ...props}) => {
+export const DeleteTemplate: FC<IDeleteTemplateProps> = ({title, deleteTranslation, navigateTo, navigateQuery, onDelete, deleteQuery, translation = true, ...props}) => {
+	const blockContext = useBlockContext();
+	const discoveryContext = useDiscoveryContext();
+	const navigate = useNavigate();
+	const {t} = useTranslation();
 	return <Template
 		title={(translation && isString(title)) ? title + ".delete" : title}
 		translation={translation}
 		status={"error"}
+		extra={<>
+			<Button
+				danger
+				size={"large"}
+				icon={<DeleteItemIcon/>}
+				onClick={() => {
+					blockContext.block();
+					onDelete(discoveryContext, deleteQuery)
+						.on("response", data => {
+							navigate(navigateTo, navigateQuery);
+							message.success(t(deleteTranslation + ".delete.success", {data}));
+						})
+						.on("catch", () => {
+							message.error(t(deleteTranslation + ".delete.error"));
+						});
+				}}
+			>
+				{t(deleteTranslation + ".delete.submit")}
+			</Button>
+			{props.children ? <Divider/> : null}
+		</>}
 		{...props}
 	/>;
 };
