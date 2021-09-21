c69bba9e13829a017666c5ad6ce0c57bac3e9dd2
Marek Hanzal <marek.hanzal@x32.cz>
2021-09-21 18:40:12 +0200

    Common: Still improving things bit by bit

--- src/query/utils.ts
+++ src/query/utils.ts
@@ -3,12 +3,12 @@ import {useMutation, useQuery} from "react-query";
 
 export function wrapQuery<TQuery extends IQueryParams = IQueryParams, TRequest = any, TResponse = any>(link: string, promise: IPromiseQueryCallback<TQuery, TRequest, TResponse>): IQueryHookCallback<TQuery, TRequest, TResponse> {
 	return (request: TRequest, query?: TQuery, options?: IQueryOptions<TResponse>) => {
-		return useQuery([link, {query, request}], promise(link, request, query), options);
+		return useQuery(["query", link, {query, request}], promise(link, request, query), options);
 	};
 }
 
 export function wrapMutation<TQuery extends IQueryParams = IQueryParams, TRequest = any, TResponse = any>(link: string, promise: IPromiseMutationCallback<TQuery, TRequest, TResponse>): IMutationHookCallback<TQuery, TRequest, TResponse> {
 	return (request: TRequest, query?: TQuery, options?: IMutationOptions<TResponse, undefined, undefined, undefined>) => {
-		return useMutation([link, {query, request}], promise(link, request, query), options);
+		return useMutation(["mutation", link, {query, request}], promise(link, request, query), options);
 	};
 }
