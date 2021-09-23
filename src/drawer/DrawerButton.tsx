4e8f4827ae9568d6668ece4bbd068baa5b6303a5
Marek Hanzal <marek.hanzal@x32.cz>
2021-09-24 00:06:12 +0200

    Common: Even more improved drawer button

--- src/drawer/DrawerButton.tsx
+++ src/drawer/DrawerButton.tsx
@@ -1,8 +1,11 @@
 import {useDrawerContext} from "@leight-core/leight";
 import {Button, ButtonProps} from "antd";
-import {FC} from "react";
+import {FC, ReactNode} from "react";
+import {useTranslation} from "react-i18next";
 
 export interface IDrawerButtonProps extends Partial<ButtonProps> {
+	label?: ReactNode;
+	title?: string;
 	/**
 	 * Optional drawer width.
 	 */
@@ -12,10 +15,13 @@ export interface IDrawerButtonProps extends Partial<ButtonProps> {
 /**
  * Default Antd button without any preset; just the drawer is shown on click.
  */
-export const DrawerButton: FC<IDrawerButtonProps> = ({children, width, ...props}) => {
+export const DrawerButton: FC<IDrawerButtonProps> = ({children, label, title, width, ...props}) => {
 	const drawerContext = useDrawerContext();
+	const {t} = useTranslation();
 	return <Button
 		onClick={() => drawerContext.display(children, width)}
 		{...props}
-	/>;
+	>
+		{title ? t(title) : label}
+	</Button>;
 };
