80d1ce2fb82096ec6f3b598a0c9eb4d0f60a9b99
Marek Hanzal <marek.hanzal@x32.cz>
2021-09-24 00:26:04 +0200

    Common: ...

--- src/drawer/DrawerButton.tsx
+++ src/drawer/DrawerButton.tsx
@@ -1,5 +1,5 @@
 import {useDrawerContext} from "@leight-core/leight";
-import {Button, ButtonProps, Tooltip} from "antd";
+import {Button, ButtonProps} from "antd";
 import {FC, ReactNode} from "react";
 import {useTranslation} from "react-i18next";
 
@@ -19,15 +19,10 @@ export interface IDrawerButtonProps extends Partial<ButtonProps> {
 export const DrawerButton: FC<IDrawerButtonProps> = ({children, label, title, tooltip, width, ...props}) => {
 	const drawerContext = useDrawerContext();
 	const {t} = useTranslation();
-
-	const ButtonInternal = () => {
-		return <Button
-			onClick={() => drawerContext.display(children, width)}
-			{...props}
-		>
-			{title ? t(title) : label}
-		</Button>;
-	};
-
-	return tooltip ? <Tooltip title={t(tooltip)}><ButtonInternal/></Tooltip> : <ButtonInternal/>;
+	return <Button
+		onClick={() => drawerContext.display(children, width)}
+		{...props}
+	>
+		{title ? t(title) : label}
+	</Button>;
 };
