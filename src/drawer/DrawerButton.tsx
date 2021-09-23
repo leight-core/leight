cd5f71541295976df3dab8fee3182a190c25911a
Marek Hanzal <marek.hanzal@x32.cz>
2021-09-24 00:21:35 +0200

    Common: Even more cool drawer button

--- src/drawer/DrawerButton.tsx
+++ src/drawer/DrawerButton.tsx
@@ -1,5 +1,5 @@
 import {useDrawerContext} from "@leight-core/leight";
-import {Button, ButtonProps} from "antd";
+import {Button, ButtonProps, Tooltip} from "antd";
 import {FC, ReactNode} from "react";
 import {useTranslation} from "react-i18next";
 
@@ -10,18 +10,24 @@ export interface IDrawerButtonProps extends Partial<ButtonProps> {
 	 * Optional drawer width.
 	 */
 	width?: number;
+	tooltip?: string;
 }
 
 /**
  * Default Antd button without any preset; just the drawer is shown on click.
  */
-export const DrawerButton: FC<IDrawerButtonProps> = ({children, label, title, width, ...props}) => {
+export const DrawerButton: FC<IDrawerButtonProps> = ({children, label, title, tooltip, width, ...props}) => {
 	const drawerContext = useDrawerContext();
 	const {t} = useTranslation();
-	return <Button
-		onClick={() => drawerContext.display(children, width)}
-		{...props}
-	>
-		{title ? t(title) : label}
-	</Button>;
+
+	const ButtonInternal = () => {
+		return <Button
+			onClick={() => drawerContext.display(children, width)}
+			{...props}
+		>
+			{title ? t(title) : label}
+		</Button>;
+	};
+
+	return tooltip ? <Tooltip title={t(tooltip)}><ButtonInternal/></Tooltip> : <ButtonInternal/>;
 };
