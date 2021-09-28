e4e71bb77307625f5934eac181f675ce417aeca0
Marek Hanzal <marek.hanzal@x32.cz>
2021-09-28 18:22:04 +0200

    Common: Improved Table API a bit

--- src/table/Table.tsx
+++ src/table/Table.tsx
@@ -1,19 +1,25 @@
-import {IRecordItem, isCallable, ITableChildrenCallback, LoaderIcon, useSourceContext} from "@leight-core/leight";
+import {IQueryParams, IRecordItem, isCallable, ISourceContext, ITableChildrenCallback, LoaderIcon, useSourceContext} from "@leight-core/leight";
 import {Table as CoolTable, TableProps} from "antd";
 import {ColumnProps} from "antd/lib/table";
+import {ReactNode} from "react";
 import {useTranslation} from "react-i18next";
 
-export interface ITableProps<TItem extends Object> extends TableProps<TItem> {
-	children?: ITableChildrenCallback<TItem>;
+export interface ITableProps<TQuery extends IQueryParams, TResponse extends IRecordItem, TOrderBy, TFilter> extends TableProps<TResponse> {
+	header?: (sourceContext?: ISourceContext<TQuery, TResponse, TOrderBy, TFilter>) => ReactNode;
+	children?: ITableChildrenCallback<TResponse> | ReactNode;
 }
 
-export const Table = <TItem extends Object = IRecordItem>(
+export const Table = <TQuery extends IQueryParams, TResponse extends IRecordItem, TOrderBy, TFilter>(
 	{
 		children,
+		header,
 		...props
-	}: ITableProps<TItem>) => {
+	}: ITableProps<TQuery, TResponse, TOrderBy, TFilter>) => {
 	const {t} = useTranslation();
-	const sourceContext = useSourceContext<any, TItem, any, any>();
+	const sourceContext = useSourceContext<TQuery, TResponse, TOrderBy, TFilter>();
+	if (header && !props.title) {
+		props.title = () => header(sourceContext);
+	}
 	return <CoolTable
 		style={{minHeight: "50vh"}}
 		dataSource={sourceContext.result.isSuccess ? sourceContext.result.data.items : []}
@@ -30,11 +36,11 @@ export const Table = <TItem extends Object = IRecordItem>(
 		}}
 		{...props}
 	>
-		{isCallable(children) ? (children as ITableChildrenCallback<TItem>)(props => {
+		{isCallable(children) ? (children as ITableChildrenCallback<TResponse>)(props => {
 			if (props.title) {
 				props.title = t(props.title as string);
 			}
-			return <CoolTable.Column<TItem> {...(props as ColumnProps<TItem>)}/>;
+			return <CoolTable.Column<TResponse> {...(props as ColumnProps<TResponse>)}/>;
 		}) : children}
 	</CoolTable>;
 };
