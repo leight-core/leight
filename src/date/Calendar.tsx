948c84bda8b759f7071754b371e8af874ce6bd2d
Marek Hanzal <marek.hanzal@x32.cz>
2021-11-02 13:03:14 +0100

    Common: Just reformat

--- src/date/Calendar.tsx
+++ src/date/Calendar.tsx
@@ -1,5 +1,4 @@
 import generateCalendar from "antd/lib/calendar/generateCalendar";
-// import "antd/lib/calendar/style";
 import {Dayjs} from "dayjs";
 import dayjsGenerateConfig from "rc-picker/lib/generate/dayjs";
 
