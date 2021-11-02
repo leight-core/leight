2bb940e6de31c49c59515589d31b4ba7da87aec9
Marek Hanzal <marek.hanzal@x32.cz>
2021-11-02 13:25:27 +0100

    Common: Finding what's wrong in leight...

--- src/date/Calendar.tsx
+++
@@ -1,5 +0,0 @@
-import generateCalendar from "antd/lib/calendar/generateCalendar";
-import {Dayjs} from "dayjs";
-import dayjsGenerateConfig from "rc-picker/lib/generate/dayjs";
-
-export const Calendar = generateCalendar<Dayjs>(dayjsGenerateConfig);
