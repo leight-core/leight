382958acf3475ac17f49d5580fe433585738ee32
Marek Hanzal <marek.hanzal@x32.cz>
2021-11-05 12:45:54 +0100

    Common: Lowered restrictiveness in HeaderPortal

--- src/page/HeaderPortal.tsx
+++ src/page/HeaderPortal.tsx
@@ -7,8 +7,5 @@ export interface IHeaderPortalProps {
 
 export const HeaderPortal: FC<IHeaderPortalProps> = ({children}) => {
 	const pageContext = usePageContext();
-	if (!pageContext.headerEl) {
-		throw new Error("Cannot use HeaderPortal, because PageContext does not have any 'headerRef' specified. You can use <PageHeader/> component (before usage of HeaderPortal) which provides automatic support for HeaderPortal.");
-	}
-	return createPortal(children, pageContext.headerEl);
+	return pageContext.headerEl ? createPortal(children, pageContext.headerEl) : null;
 };
