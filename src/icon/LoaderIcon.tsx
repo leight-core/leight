843cf6446e6469963d53c8bf2724ff7777a81bef
Marek Hanzal <marek.hanzal@x32.cz>
2021-12-06 14:22:58 +0100

    Common: Changed loading icon... again, again and again

--- src/icon/LoaderIcon.tsx
+++ src/icon/LoaderIcon.tsx
@@ -1,5 +1,5 @@
-import {SyncOutlined} from "@ant-design/icons";
+import {LoadingOutlined} from "@ant-design/icons";
 import {IIconProps} from "@leight-core/leight";
 import {FC} from "react";
 
-export const LoaderIcon: FC<IIconProps> = props => <SyncOutlined spin {...props}/>;
+export const LoaderIcon: FC<IIconProps> = props => <LoadingOutlined spin {...props}/>;
