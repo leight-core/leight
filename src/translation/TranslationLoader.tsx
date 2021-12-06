7288e3397569cd55e3b0141826049aaed74a069b
Marek Hanzal <marek.hanzal@x32.cz>
2021-12-06 14:42:04 +0100

    Common: Improved translation loader

--- src/translation/TranslationLoader.tsx
+++ src/translation/TranslationLoader.tsx
@@ -1,6 +1,6 @@
 import {TranslationOutlined} from "@ant-design/icons";
 import {LoaderLayout, useI18NextContext} from "@leight-core/leight";
-import {FC, ReactNode, useEffect} from "react";
+import {FC, ReactNode, useEffect, useState} from "react";
 import {useTranslationQuery} from "./useTranslationQuery";
 
 export interface ITranslationLoaderProps {
@@ -14,12 +14,14 @@ export interface ITranslationLoaderProps {
 export const TranslationLoader: FC<ITranslationLoaderProps> = ({logo, link, children}) => {
 	const result = useTranslationQuery(link);
 	const {i18next} = useI18NextContext();
+	const [isLoading, setIsLoading] = useState(true);
 	useEffect(() => {
-		result.isSuccess && result.data.translations.forEach(translation => i18next.addResource(translation.language, "translation", translation.label, translation.text));
-	}, [result.isSuccess, result.data]);
+		result.isSuccess && result.data.translations.forEach(translation => i18next.addResource(translation.language, "translation", translation.label, translation.text)) && setIsLoading(false);
+	}, [result.data]);
 	return <LoaderLayout
 		logo={logo}
 		icon={<TranslationOutlined/>}
+		loading={isLoading}
 		queryResult={result}
 		errorText={"Translations cannot be loaded."}
 	>
