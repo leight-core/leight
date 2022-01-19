68c39d68162868290a5d0d62d509466d83e893e4
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-19 13:19:24 +0100

    Common: Improved bullet card

--- src/component/BulletCard.tsx
+++ src/component/BulletCard.tsx
@@ -1,10 +1,10 @@
 import {RightCircleOutlined} from "@ant-design/icons";
 import {NumberRange} from "@leight-core/leight";
-import {Result, Typography} from "antd";
+import {Result, ResultProps, Typography} from "antd";
 import {FC, ReactNode} from "react";
 import {useTranslation} from "react-i18next";
 
-export interface IBulletCardProps {
+export interface IBulletCardProps extends ResultProps {
 	/**
 	 * Title of the bullet card; uses translation.
 	 */
@@ -17,6 +17,7 @@ export interface IBulletCardProps {
 	 * Optional icon (used in Result under the hood).
 	 */
 	icon: boolean | ReactNode;
+	bullet: (() => ReactNode) | undefined;
 }
 
 /**
@@ -37,16 +38,20 @@ export const BulletCard: FC<IBulletCardProps> = (
 		title,
 		count,
 		icon = false,
+		bullet = () => <RightCircleOutlined style={{color: "#1890ff"}}/>,
+		...props
 	}) => {
 	const {t} = useTranslation();
 	return <Result
 		icon={icon === false ? <></> : icon}
-		title={t(title)}
+		title={t(title + ".title")}
+		subTitle={t(title + ".subtitle")}
 		style={{paddingTop: 0}}
+		{...props}
 	>
 		{NumberRange(count).map(index => (
 			<Typography.Paragraph key={index}>
-				<RightCircleOutlined style={{color: "#1890ff"}}/>&nbsp;{t(title + ".list.item-" + index)}
+				{bullet()}&nbsp;{t(title + ".list.item-" + index)}
 			</Typography.Paragraph>
 		))}
 	</Result>;
