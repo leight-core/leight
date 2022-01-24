5a45741e0fc519e8b76c24795469bf8c991483b2
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-24 13:33:36 +0100

    Common: Updated icon

--- src/menu/QuickMenu.tsx
+++ src/menu/QuickMenu.tsx
@@ -1,4 +1,4 @@
-import {ContextMenuIcon} from "@leight-core/leight";
+import {MoreOutlined} from "@ant-design/icons";
 import {Dropdown, DropDownProps, Menu} from "antd";
 import {FC, ReactNode} from "react";
 
@@ -6,7 +6,7 @@ export interface IQuickMenuProps extends Partial<DropDownProps> {
 	icon?: ReactNode;
 }
 
-export const QuickMenu: FC<IQuickMenuProps> = ({icon = <ContextMenuIcon/>, children, ...props}) => {
+export const QuickMenu: FC<IQuickMenuProps> = ({icon = <MoreOutlined/>, children, ...props}) => {
 	return <Dropdown
 		trigger={["click"]}
 		overlay={() => <Menu>{children}</Menu>}
