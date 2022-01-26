ee2cf07118aa538c81c8983f48781ce3877b5338
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-26 17:58:08 +0100

    Common: Fixed time picker

--- src/date/TimePicker.tsx
+++ src/date/TimePicker.tsx
@@ -1,8 +1,7 @@
 import {DatePicker} from "@leight-core/leight";
-import {PickerTimeProps} from "antd/lib/date-picker/generatePicker";
-import {Moment} from "moment";
+import {TimePickerProps} from "antd";
 import React, {forwardRef} from "react";
 
-export const TimePicker = forwardRef<any, Omit<PickerTimeProps<Moment>, "picker">>((props, ref) => <DatePicker {...props} picker={"time"} mode={undefined} ref={ref}/>);
+export const TimePicker = forwardRef<any, Omit<TimePickerProps, "format">>((props, ref) => <DatePicker {...props} picker={"time"} mode={undefined} ref={ref}/>);
 
 TimePicker.displayName = "TimePicker";
