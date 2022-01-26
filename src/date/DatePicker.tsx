2df757254c1712bfd290ace6d4218f93f62f9129
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-26 17:51:00 +0100

    Common: Improved picker

--- src/date/DatePicker.tsx
+++ src/date/DatePicker.tsx
@@ -2,11 +2,11 @@ import {useOptionalFormItemContext} from "@leight-core/leight";
 import {DatePicker as CoolDatePicker, DatePickerProps as CoolDatePickerProps} from "antd";
 import {forwardRef} from "react";
 
-export const DatePicker = forwardRef<any, CoolDatePickerProps>((props, ref) => {
+export const DatePicker = forwardRef<any, CoolDatePickerProps & { format?: string }>(({format = "LLL", ...props}, ref) => {
 	const formItemContext = useOptionalFormItemContext();
 	return <CoolDatePicker
 		ref={ref}
-		format={date => date.format("LLL")}
+		format={date => date.format(format || "LLL")}
 		{...props}
 		placeholder={formItemContext && (props as any).usePlaceholder ? formItemContext.label : undefined}
 	/>;
