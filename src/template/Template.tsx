0f9b45ad5617e3858497cc1cc6becbfe45141a77
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-27 17:31:53 +0100

    Common: Improved template a bity

--- src/template/Template.tsx
+++ src/template/Template.tsx
@@ -7,13 +7,13 @@ import {useTranslation} from "react-i18next";
 export interface ITemplateProps extends Partial<ResultProps> {
 	label?: string;
 	span?: number;
-	forMobile?: boolean;
+	isMobile?: boolean;
 }
 
-export const Template: FC<ITemplateProps> = ({icon, label, title, subTitle, span = 16, forMobile = false, children, ...props}) => {
+export const Template: FC<ITemplateProps> = ({icon, label, title, subTitle, span = 16, isMobile = false, children, ...props}) => {
 	const {t} = useTranslation();
 	return <>
-		{(isBrowser || forMobile) && <Result
+		{(isBrowser || isMobile) && <Result
 			icon={icon}
 			title={(title || title === false) ? title : (label ? t(label + ".title") : label)}
 			subTitle={(subTitle || subTitle === false) ? subTitle : (label ? t(label + ".subtitle") : label)}
