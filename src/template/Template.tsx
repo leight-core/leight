6f0455fe7abc890274296e43c87bf8f33b2c9ec6
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-27 17:05:36 +0100

    Common: Improved template view

--- src/template/Template.tsx
+++ src/template/Template.tsx
@@ -1,7 +1,7 @@
 import {Centered} from "@leight-core/leight";
 import {Divider, Result, ResultProps} from "antd";
 import {FC} from "react";
-import {isMobile} from "react-device-detect";
+import {isBrowser} from "react-device-detect";
 import {useTranslation} from "react-i18next";
 
 export interface ITemplateProps extends Partial<ResultProps> {
@@ -12,15 +12,15 @@ export interface ITemplateProps extends Partial<ResultProps> {
 export const Template: FC<ITemplateProps> = ({icon, label, title, subTitle, span = 16, children, ...props}) => {
 	const {t} = useTranslation();
 	return <>
-		<Result
+		{isBrowser && <Result
 			icon={icon}
 			title={(title || title === false) ? title : (label ? t(label + ".title") : label)}
 			subTitle={(subTitle || subTitle === false) ? subTitle : (label ? t(label + ".subtitle") : label)}
 			extra={<Divider/>}
 			{...props}
-		/>
-		<Centered span={isMobile ? 24 : span}>
+		/>}
+		{isBrowser ? <Centered span={span}>
 			{children}
-		</Centered>
+		</Centered> : children}
 	</>;
 };
