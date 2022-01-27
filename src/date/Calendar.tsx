8ba1d40daa517efd5dda56599b3d9c475aca58f1
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-27 09:54:14 +0100

    Common: Updated packages and removed DateTime components as they did not work properly

--- src/date/Calendar.tsx
+++
@@ -1,9 +0,0 @@
-import {Calendar as CoolCalendar, CalendarProps as CoolCalendarProps} from "antd";
-import {Moment} from "moment";
-import {FC} from "react";
-
-export const Calendar: FC<CoolCalendarProps<Moment>> = props => {
-	return <CoolCalendar
-		{...props}
-	/>;
-};
--- src/date/TimePicker.tsx
+++
@@ -1,7 +0,0 @@
-import {DatePicker} from "@leight-core/leight";
-import {TimePickerProps} from "antd";
-import React, {forwardRef} from "react";
-
-export const TimePicker = forwardRef<any, Omit<TimePickerProps, "format">>((props, ref) => <DatePicker {...props} picker={"time"} mode={undefined} ref={ref}/>);
-
-TimePicker.displayName = "TimePicker";
