4054e1f1348f8b74ce4a9d4c91c331ddfc034f2a
Marek Hanzal <marek.hanzal@x32.cz>
2022-01-31 09:07:32 +0100

    Common: Added back date placeholder

--- src/date/DatePicker.tsx
+++ src/date/DatePicker.tsx
@@ -1,15 +1,19 @@
 import {DatePicker as CoolDatePicker, DatePickerProps} from "antd";
 import {FC} from "react";
+import {useOptionalFormItemContext} from "../form";
 
 export type IDatePickerProps = {
 	format?: string
+	usePlaceholder?: boolean
 }
 
-export const DatePicker: FC<DatePickerProps & IDatePickerProps> = ({format = "LLL", ...props}) => {
+export const DatePicker: FC<DatePickerProps & IDatePickerProps> = ({format = "LLL", usePlaceholder = false, ...props}) => {
+	const formItemContext = useOptionalFormItemContext();
 	return <CoolDatePicker
 		format={date => date.format(format)}
 		size={"large"}
 		style={{width: "100%"}}
+		placeholder={formItemContext && (props as any).usePlaceholder ? formItemContext.label : undefined}
 		{...props}
 	/>;
 };
