d3e46f3ba151eea5463504c9bf6d8e3d1a603c78
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-02 11:53:10 +0100

    Common: Improved copy text component

--- src/utils/CopyText.tsx
+++ src/utils/CopyText.tsx
@@ -3,30 +3,20 @@ import {FC} from "react";
 import {useTranslation} from "react-i18next";
 
 export interface ICopyTextProps {
-	/**
-	 * Optional text to copy
-	 */
-	text?: string | null;
-	copy?: string | null;
-	/**
-	 * Default text
-	 */
-	defaultText?: string;
+	copy: string;
 	/**
 	 * Translation label for copy tooltips
 	 */
-	label: string;
+	tooltip: string;
 }
 
-export const CopyText: FC<ICopyTextProps> = ({text, label, copy = text, defaultText = "-"}) => {
+export const CopyText: FC<ICopyTextProps> = ({children, tooltip, copy}) => {
 	const {t} = useTranslation();
-	return <>
-		{(text && copy) ? <Typography.Text
-			copyable={{
-				text: copy,
-				tooltips: [t(label + ".copy"), t(label + ".copied")]
-			}}
-			children={text}
-		/> : defaultText}
-	</>;
+	return <Typography.Text
+		copyable={{
+			text: copy,
+			tooltips: [t(tooltip + ".copy"), t(tooltip + ".copied")]
+		}}
+		children={children}
+	/>;
 };
