7f0c3e720946b48b0fda535aa90fd2fd88aa94c2
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-02 10:23:59 +0100

    Common: Added Filter component

---
+++ src/form/Filter.tsx
@@ -0,0 +1,74 @@
+import {CloseCircleOutlined, SearchOutlined} from "@ant-design/icons";
+import {Centered, DrawerButton, DrawerContext, Form, Submit, useFormContext} from "@leight-core/leight";
+import {Button, Divider, Space} from "antd";
+import {FC, PropsWithChildren} from "react";
+import {useTranslation} from "react-i18next";
+
+interface IFilterInternalProps {
+	onClear: () => void;
+}
+
+const FilterInternal: FC<IFilterInternalProps> = ({onClear, children}) => {
+	const {t} = useTranslation();
+	const formContext = useFormContext();
+	return <>
+		{children}
+		<Divider/>
+		<Centered>
+			<Space align={"baseline"} split={<Divider type={"vertical"}/>}>
+				<Button
+					size={"middle"}
+					onClick={() => {
+						formContext.reset();
+						onClear();
+					}}
+					icon={<CloseCircleOutlined/>}
+				>
+					{t("common.filter.clear")}
+				</Button>
+				<Submit
+					icon={<SearchOutlined/>}
+					label={"common.filter.submit"}
+				/>
+			</Space>
+		</Centered>
+	</>;
+};
+
+export interface IFilterProps<TFilter = any> {
+	filter: TFilter;
+	translation: string;
+	onFilter: (filter: TFilter) => void;
+	onClear: () => void;
+}
+
+export type IFilterWithoutTranslationProps<TFilter = any> = Omit<IFilterProps<TFilter>, "translation">;
+
+export function Filter<TFilter = any, >({filter, translation, onFilter, onClear, ...props}: PropsWithChildren<IFilterProps<TFilter>>): JSX.Element {
+	return <DrawerButton
+		icon={<SearchOutlined/>}
+		type={"link"}
+		size={"small"}
+		title={translation + ".filter.title"}
+		label={translation + ".filter.title"}
+		width={600}
+	>
+		<DrawerContext.Consumer>
+			{drawerContext => <Form
+				toForm={() => filter}
+				onSuccess={({response}) => {
+					onFilter(response);
+					drawerContext.setVisible(false);
+				}}
+			>
+				<FilterInternal
+					onClear={() => {
+						drawerContext && drawerContext.setVisible(false);
+						onClear && onClear();
+					}}
+					{...props}
+				/>
+			</Form>}
+		</DrawerContext.Consumer>
+	</DrawerButton>;
+}
