ac8b624c1f6b9ace5e36fd1a0634774706879fb9
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-03 14:15:15 +0100

    Common: Added optional filter context

--- src/form/FilterContext.tsx
+++ src/form/FilterContext.tsx
@@ -1,4 +1,4 @@
-import {IFilterContext, useContext} from "@leight-core/leight";
+import {IFilterContext, useContext, useOptionalContext} from "@leight-core/leight";
 import {createContext} from "react";
 
 export const FilterContext = createContext<IFilterContext>(null as any);
@@ -7,3 +7,5 @@ export const FilterContext = createContext<IFilterContext>(null as any);
  * Access to UI blocking context of a Form.
  */
 export const useFilterContext = <TFilter, >() => useContext<IFilterContext<TFilter>>(FilterContext, "FilterContext");
+
+export const useOptionalFilterContext = <TFilter, >() => useOptionalContext<IFilterContext<TFilter>>(FilterContext as any);
