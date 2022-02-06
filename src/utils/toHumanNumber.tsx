bbd9ef02d347982a7be960a6b2e85e419021f8d2
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-06 18:48:56 +0100

    Common: Fixed utility func

--- src/utils/toHumanNumber.tsx
+++ src/utils/toHumanNumber.tsx
@@ -1,5 +1,13 @@
-export function toHumanNumber(number: number, max = 2): string {
-	return number.toLocaleString(undefined, {
-		maximumSignificantDigits: max,
-	});
+export function toHumanNumber(number?: number | null, max = 2): string {
+	if (number === null || number === undefined) {
+		return "-";
+	}
+	try {
+		return number.toLocaleString(undefined, {
+			maximumSignificantDigits: max,
+		});
+	} catch (e) {
+		console.error("toHumanNumber", number, e);
+		return number.toFixed(2);
+	}
 }
