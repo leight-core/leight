816847a18c66da040ac3cb30279460efb9dfa830
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-08 17:54:40 +0100

    Common: Improved mobile drawer

--- src/menu/DrawerMenu.tsx
+++ src/menu/DrawerMenu.tsx
@@ -2,6 +2,7 @@ import {MenuOutlined} from "@ant-design/icons";
 import {DrawerButton, IDrawerButtonProps} from "@leight-core/leight";
 import {Menu} from "antd";
 import {FC, ReactNode} from "react";
+import {isMobile} from "react-device-detect";
 
 export interface IDrawerMenuProps extends Partial<IDrawerButtonProps> {
 	header?: ReactNode;
@@ -12,6 +13,7 @@ export const DrawerMenu: FC<IDrawerMenuProps> = ({children, header, ...props}) =
 		type={"text"}
 		drawerProps={{
 			title: header,
+			headerStyle: isMobile ? {padding: "8px 4px"} : undefined,
 			bodyStyle: {padding: 0}
 		}}
 		width={400}
