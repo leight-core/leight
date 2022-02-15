2e424f03943a392342af00be19c56857a4fc62d8
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-15 22:06:52 +0100

    Common: Card is translating title

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -1,15 +1,19 @@
 import {Card as CoolCard, CardProps as CoolCardProps} from "antd";
 import {FC} from "react";
+import {useTranslation} from "react-i18next";
 import {useIsMobile} from "../browser";
+import {isString} from "../utils";
 
 export interface ICardProps extends Partial<CoolCardProps> {
 }
 
-export const Card: FC<ICardProps> = props => {
+export const Card: FC<ICardProps> = ({title, ...props}) => {
 	const isMobile = useIsMobile();
+	const {t} = useTranslation();
 	return <CoolCard
 		bodyStyle={isMobile ? {padding: "8px"} : undefined}
 		headStyle={isMobile ? {minHeight: "32px"} : undefined}
+		title={isString(title) ? t(title as string) : title}
 		{...props}
 	/>;
 };
