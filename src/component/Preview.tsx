2b49d87dc7d311315995d4e74712424d2fe77d9e
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-15 17:16:32 +0100

    Common: Improved preview component

--- src/component/Preview.tsx
+++ src/component/Preview.tsx
@@ -1,7 +1,3 @@
-import {List, ListProps} from "antd";
-import {FC, ReactNode} from "react";
-import {useTranslation} from "react-i18next";
-
 export interface IPreviewProps extends Partial<ListProps<any>> {
 	translation?: string;
 	children: { [index in string]: ReactNode };
@@ -9,12 +5,18 @@ export interface IPreviewProps extends Partial<ListProps<any>> {
 
 export const Preview: FC<IPreviewProps> = ({translation, children, ...props}) => {
 	const {t} = useTranslation();
-	return <List itemLayout={"vertical"} size={"small"} {...props}>
+	return <List
+		itemLayout={"vertical"}
+		size={"small"}
+		{...props}
+	>
 		{Object.entries(children).map(([key, item]) => <List.Item
 			key={key}
 		>
-			<List.Item.Meta title={t(translation ? (translation + "." + key) : key)}/>
-			{item ? item : "-"}
+			<List.Item.Meta
+				title={t(translation ? (translation + "." + key) : key)}
+				description={item ? item : "-"}
+			/>
 		</List.Item>)}
 	</List>;
 };
