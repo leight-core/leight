73b021acd1fa50498c3a80b38b35cce81bc46ab8
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-19 22:43:26 +0100

    Common: Exported drawer placement

--- src/menu/DrawerMenu.tsx
+++ src/menu/DrawerMenu.tsx
@@ -3,18 +3,23 @@ import {DrawerButton, IDrawerButtonProps, useIsMobile} from "@leight-core/leight
 import {Menu} from "antd";
 import {FC, ReactNode} from "react";
 
+declare const PlacementTypes: ["top", "right", "bottom", "left"];
+declare type placementType = typeof PlacementTypes[number];
+
 export interface IDrawerMenuProps extends Partial<IDrawerButtonProps> {
 	header?: ReactNode;
+	placement?: placementType;
 }
 
-export const DrawerMenu: FC<IDrawerMenuProps> = ({children, header, ...props}) => {
+export const DrawerMenu: FC<IDrawerMenuProps> = ({children, header, placement, ...props}) => {
 	const isMobile = useIsMobile();
 	return <DrawerButton
 		type={"text"}
 		drawerProps={{
 			title: header,
 			headerStyle: isMobile ? {padding: "8px 4px"} : undefined,
-			bodyStyle: {padding: 0}
+			bodyStyle: {padding: 0},
+			placement,
 		}}
 		width={400}
 		icon={<MenuOutlined/>}
