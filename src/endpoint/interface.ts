51f14df138e24e1d715bd51e48248c83eaa1d06b
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-21 18:31:29 +0100

    Common: Version bump

--- src/endpoint/interface.ts
+++ src/endpoint/interface.ts
@@ -7,4 +7,4 @@ export interface INextApiRequest<TQuery extends IQueryParams, TRequest> extends 
 	body: TRequest;
 }
 
-export type IEndpoint<TRequest, TResponse, TQuery extends IQueryParams> = (req: INextApiRequest<TQuery, TRequest>, res: NextApiResponse<TResponse>) => void;
+export type IEndpoint<TRequest, TResponse, TQuery extends IQueryParams = void> = (req: INextApiRequest<TQuery, TRequest>, res: NextApiResponse<TResponse>) => void;
