c74ab1a8ac6fc01cccb3974e255e4b5d58d22e6a
Marek Hanzal <marek.hanzal@x32.cz>
2022-02-23 19:55:04 +0100

    Common: A little fix?

--- src/sdk/reflection.ts
+++ src/sdk/reflection.ts
@@ -1,12 +1,12 @@
 import {exportEndpoint, exportInterface, IEndpointReflection, IGenerators, IInterfaceReflection, ISdk, pickNodes} from "@leight-core/leight";
-import fs from "fs";
+import {readFileSync} from "fs-extra";
 import {glob} from "glob";
 
 export function toSdk(endpoint: string, generators: IGenerators): ISdk | undefined {
 	const ts = require("typescript");
 	const interfaces: (IInterfaceReflection | false)[] = [];
 	const endpoints: (IEndpointReflection | false)[] = [];
-	const root = ts.createSourceFile(endpoint, fs.readFileSync(endpoint, "utf8"), ts.ScriptTarget.Latest);
+	const root = ts.createSourceFile(endpoint, readFileSync(endpoint, "utf8"), ts.ScriptTarget.Latest);
 
 	pickNodes(["*", "InterfaceDeclaration"], root, root).forEach(node => interfaces.push(exportInterface(node, root)));
 	pickNodes(["*", "FirstStatement"], root, root).forEach(node => endpoints.push(exportEndpoint(node, root, generators)));
