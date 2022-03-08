1d640744bd4ebc25808380fa6890bcbb286be174
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-08 10:32:10 +0100

    Common: Improved bool inline

--- src/component/BoolInline.tsx
+++ src/component/BoolInline.tsx
@@ -1,17 +1,20 @@
-import {CheckCircleTwoTone, CloseCircleTwoTone, MinusCircleTwoTone} from "@ant-design/icons";
+import {CheckCircleTwoTone, CloseCircleTwoTone, MinusCircleTwoTone, TwoToneColor} from "@ant-design/icons";
 import {Avatar} from "antd";
 import {FC} from "react";
 
 export interface IBoolInlineProps {
 	bool?: boolean | null;
+	trueColor?: TwoToneColor;
+	falseColor?: TwoToneColor;
+	unknownColor?: TwoToneColor;
 }
 
-export const BoolInline: FC<IBoolInlineProps> = ({bool}) => {
+export const BoolInline: FC<IBoolInlineProps> = ({bool, trueColor = "#71da17", falseColor = "#e26738", unknownColor}) => {
 	return <Avatar
 		size={"large"}
 		style={{backgroundColor: "transparent"}}
 		icon={bool !== undefined && bool !== null ?
-			(bool ? <CheckCircleTwoTone/> : <CloseCircleTwoTone/>) :
-			<MinusCircleTwoTone/>
+			(bool ? <CheckCircleTwoTone twoToneColor={trueColor}/> : <CloseCircleTwoTone twoToneColor={falseColor}/>) :
+			<MinusCircleTwoTone twoToneColor={unknownColor}/>
 		}/>;
 };
