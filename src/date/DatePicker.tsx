2340ce9eb8d4df046803801e0d8e26565add7070
Marek Hanzal <marek.hanzal@x32.cz>
2022-03-28 15:31:06 +0200

    Common: Version bump

--- src/date/DatePicker.tsx
+++ src/date/DatePicker.tsx
@@ -1,19 +1,20 @@
 import {useOptionalFormItemContext} from "@leight-core/leight";
 import {DatePicker as CoolDatePicker, DatePickerProps} from "antd";
-import {FC} from "react";
+import {FC, forwardRef} from "react";
 
 export type IDatePickerProps = {
 	format?: string
 	usePlaceholder?: boolean
 }
 
-export const DatePicker: FC<DatePickerProps & IDatePickerProps> = ({format = "LLL", usePlaceholder = false, ...props}) => {
+export const DatePicker: FC<DatePickerProps & IDatePickerProps> = forwardRef(({format = "L", usePlaceholder = false, ...props}, ref) => {
 	const formItemContext = useOptionalFormItemContext();
 	return <CoolDatePicker
 		format={date => date.format(format)}
 		size={"large"}
 		style={{width: "100%"}}
 		placeholder={formItemContext && usePlaceholder ? formItemContext.label : undefined}
+		ref={ref as any}
 		{...props}
 	/>;
-};
+});
